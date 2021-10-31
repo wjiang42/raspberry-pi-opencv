c6199a35bae90c22336a9b956e23b2c695c2fdf8
wjiang42 <78833367+wjiang42@users.noreply.github.com>
2021-10-31 10:04:48 -0500

    Update ssd_object_detection.py

--- ssd_object_detection.py
+++ ssd_object_detection.py
@@ -6,6 +6,7 @@ import adafruit_servokit
 global image_count
 image_count = 0
 
+#orient camera
 class ServoKit(object):
     default_angle = 90
 
@@ -34,11 +35,11 @@ class ServoKit(object):
 
 servoKit = ServoKit(4)
 
-#Accessing camera
+#access camera
 cam = cv2.VideoCapture(0)
 cam.set(cv2.CAP_PROP_FPS, 7)
 
-#Model and classes
+#import model and classes
 config_path = "/home/pi/raspberry-pi-opencv/yolov3-tiny.cfg"
 weights_path = "/home/pi/raspberry-pi-opencv/yolov3-tiny.weights"
 min_prob = 0.2
@@ -50,7 +51,7 @@ net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
 while True:
     start = time.time()
     ret, image = cam.read()
-    (h, w) = image.shape[:2]
+    (h, w) = image.shape[:2] #process image
     blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (128, 128), swapRB=True, crop=False)
     net.setInput(blob)
     detections = net.forward()
@@ -63,7 +64,7 @@ while True:
         scores = output[5:]
         classID = np.argmax(scores)
         confidence = scores[classID]
-        if confidence > 0.2:
+        if confidence > 0.2: #filter detections
             box = output[0:4] * np.array([w, h, w, h])
             (centerX, centerY, width, height) = box.astype("int")
             x = int(centerX - (width / 2))
@@ -73,19 +74,19 @@ while True:
             classIDs.append(classID)
     idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.2, 0.3)
     if len(idxs) > 0:
-        for i in idxs.flatten():
+        for i in idxs.flatten(): #draw detections
             (x, y) = (boxes[i][0], boxes[i][1])
             (w, h) = (boxes[i][2], boxes[i][3])
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 250, 180), 2)
             text = "{}: {:.4f}".format(CLASSES[classIDs[i]], confidences[i])
             cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 250, 180), 2)
-            if CLASSES[classIDs[i]] == "person":
+            if CLASSES[classIDs[i]] == "person": #filter detections for a specific object
                 mx = int(x + (w/2))
                 my = int(y + (h/2))
                 cv2.rectangle(image, (mx, my), (mx + 1, my + 1), (0, 250, 180), 2)
                 cx = 640/2
                 cy = 480/2
-                motor_step = 2
+                motor_step = 2 #rotate camera to be roughly centered on object
                 if abs(mx-cx) > 20:
                     if mx < cx:
                         servoKit.setAngle(1, servoKit.getAngle(1) - motor_step)
